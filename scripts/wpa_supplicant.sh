ec709dfaa109d7b83ba0a5a702e0aaa482f07a1b
Alifi Chiganjati <alifichiganjati@gmail.com>
2026-06-14 10:44:27 +0700

    FEAT: add change service dinit

--- scripts/wpa_supplicant.sh
+++ scripts/wpa_supplicant.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+while ! ip link show wlp4s0 >/dev/null 2>&1
+do
+    sleep 1
+done
+
 . /etc/dinit.d/config/wpa_supplicant.conf
 exec /usr/bin/wpa_supplicant -i "${WPA_INTERFACE}" -c /etc/wpa_supplicant/wpa_supplicant-"${WPA_INTERFACE}".conf
